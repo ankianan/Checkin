b842e2d241f2000d078d6c7cc4e317628de15546
Ankit Anand <ankit.anand@oracle.com>
2019-05-26 01:40:12 +0530

    reverted changes in App-header

--- src/app-header/AppHeader.js
+++ src/app-header/AppHeader.js
@@ -17,10 +17,10 @@ customElements.define('app-header', class extends XHyperElement{
 			<h1 style="flex : 1;">
 				<a style="text-decoration:none; color: inherit" href="/">Checkin</a>
 			</h1>
-			<a class="${!this.props.allCheckins?'selected':''}" href="javascript:void(0)" onclick="${this.props.toggleAllCheckins}">Nearby</a>
+			${this.props.person?html`<a class="${!this.props.allCheckins?'selected':''}" href="javascript:void(0)" onclick="${this.props.toggleAllCheckins}">Nearby</a>
 			<a class="${this.props.allCheckins?'selected':''}" href="javascript:void(0)" onclick="${this.props.toggleAllCheckins}">All</a>
 			<span style="margin : 1rem;">${this.props.person.name()}</span>
-			<img class="avatar"  src="${this.props.person.avatarUrl()}" />
+			<img class="avatar"  src="${this.props.person.avatarUrl()}" />`:''}
 		</div>`
 	}
 });
